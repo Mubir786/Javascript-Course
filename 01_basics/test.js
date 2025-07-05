6d4652ae09872cc4dff7d4461525c79b35f10c5d
Mubir Hussain <117667092+Mubir786@users.noreply.github.com>
2025-07-05 05:38:05 +0000

    changes config

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("Mubir Hussain")
+let name = "Mubir Hussain";
+const age = "21";
+var city = "Karak";
+
+function details()
+{
+    console.log("Name = " + name);
+    console.log("Age = " + age);
+    console.log("City = " + city);
+
+}
+
+details()
