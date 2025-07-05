7d3ff7a5091ca2c03165340a2fe15ac573748252
Mubir Hussain <117667092+Mubir786@users.noreply.github.com>
2025-07-05 07:16:12 +0000

    changes config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) // we are using node js not browser it can give 6 on browser but give error on node js
+let name = "Mubir Hussain"
+let age = 22
+let State = null
+
+console.log(3+3)
+console.log(typeof "Mubir Hussain")
+console.log(typeof 22);
+console.log(typeof null);
+
