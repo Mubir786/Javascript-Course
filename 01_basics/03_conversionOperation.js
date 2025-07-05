b4718fbd571c63ddefb7f8ed07db618905b21051
Mubir Hussain <117667092+Mubir786@users.noreply.github.com>
2025-07-05 08:03:46 +0000

    03_conversionOperation config

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+let score5 = 33    //it was print as number
+let score4 = "33" // it was print as string
+let score3 = null
+let score2 = "33"
+let score1 = true
+let score6 = false
+
+// for Score 1
+console.log("For Score 1");
+console.log(typeof score1);
+console.log(typeof (score1));
+
+let valueInNumber1 = Number(score1)
+console.log(typeof score1);
+console.log(valueInNumber1);
+
+// for Score 2
+console.log("For Score 2");
+console.log(typeof score2);
+console.log(typeof (score2));
+
+let valueInNumber2 = Number(score2)
+console.log(typeof score2);
+console.log(valueInNumber2);
+
+// for Score 3
+console.log("For Score 3");
+console.log(typeof score3);
+console.log(typeof (score3));
+
+let valueInNumber3 = Number(score3)
+console.log(typeof score3);
+console.log(valueInNumber3);
+
+// for Score 4
+console.log("For Score 4");
+console.log(typeof score4);
+console.log(typeof (score4));
+
+let valueInNumber4 = Number(score4)
+console.log(typeof score4);
+console.log(valueInNumber4);
+
+// for Score 5
+console.log("For Score 5");
+console.log(typeof score5);
+console.log(typeof (score5));
+
+let valueInNumber5 = Number(score5)
+console.log(typeof score5);
+console.log(valueInNumber5);
+
+// for Score 6
+console.log("For Score 6");
+console.log(typeof score6);
+console.log(typeof (score6));
+
+let valueInNumber6 = Number(score6)
+console.log(typeof score6);
+console.log(valueInNumber6);
+//NaN stands for not a number
+
+ 
